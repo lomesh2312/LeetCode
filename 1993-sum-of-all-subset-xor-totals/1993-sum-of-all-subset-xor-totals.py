bc5cad9b7422cfa29428ed72f34929b122f93d44
lomesh2312 <lomeshsonkeshriya@gmail.com>
2025-10-18 10:51:13 +0530

    Time: 0 ms (100%), Space: 17.7 MB (57.52%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,6 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        total_or = 0
+        for num in nums:
+            total_or |= num
+        return total_or * (1 << (len(nums) - 1))
